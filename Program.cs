b6214fe0d41ce8815ca03b8719a80ae12f3f61f6
Reza Eskandari <rezaeskandari.ammori@yahoo.com>
2023-02-14 17:57:39 +0100

    gifs and text anime

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 using Npgsql;
 using System.Media;
-
+//using Spectre.Console;
 namespace DBTest;
 
 class Program
 {
     static void Main(string[] args)
     {
-
+        //AnsiConsole.Markup("[underline red]Hello[/] World!");
+        //Console.ReadKey();
         StartProgramBase.ProgramStart();
 
 
